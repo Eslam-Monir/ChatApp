5bfe08121e52d0fe0dc8326e9a3ede47725abda9
AzdasherMohamed <azdashermohamed78@gmail.com>
2022-04-24 22:21:49 +0200

    class story , massage and chatroom

--- src/Chatapp/Chatroom.java
+++ src/Chatapp/Chatroom.java
@@ -1,4 +1,61 @@
 package Chatapp;
 
 public class Chatroom {
+    private int id;
+    private boolean isgroup;
+    private String last_seen;
+    //vector<User> users;
+    //vector<Message>messages;
+    private String cr_desc;
+
+    public Chatroom(int id, boolean isgroup, String last_seen, String cr_desc) {
+        this.id = id;
+        this.isgroup = isgroup;
+        this.last_seen = last_seen;
+        this.cr_desc = cr_desc;
+    }
+
+    //setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setIsgroup(boolean isgroup) {
+        this.isgroup = isgroup;
+    }
+
+    public void setLast_seen(String last_seen) {
+        this.last_seen = last_seen;
+    }
+
+    public void setCr_desc(String cr_desc) {
+        this.cr_desc = cr_desc;
+    }
+//getters
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isIsgroup() {
+        return isgroup;
+    }
+
+    public String getLast_seen() {
+        return last_seen;
+    }
+
+    public String getCr_desc() {
+        return cr_desc;
+    }
+
+    public void showUsers(){};
+
+    public String showLastSeen(){
+        return "";
+    };
+
+    public void loadMessages(){};
+
+
 }
--- src/Chatapp/Message.java
+++ src/Chatapp/Message.java
@@ -1,4 +1,69 @@
 package Chatapp;
 
 public class Message {
+    private int id;
+    private User sender;
+    private String text;
+    private String date;
+    private String time;
+    private boolean seen;
+
+    public Message(int id, User sender, String text, String date, String time, boolean seen) {
+        this.id = id;
+        this.sender = sender;
+        this.text = text;
+        this.date = date;
+        this.time = time;
+        this.seen = seen;
+    }
+    //setter
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setSeen(boolean seen) {
+        this.seen = seen;
+    }
+    //getters
+
+    public int getId() {
+        return id;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public boolean isSeen() {
+        return seen;
+    }
 }
